c053c05c81ef6999a3415cc49c265526f7a7b895
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-12 14:13:57 +0200

    método isWinner

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class GameService {
 
   public board: any = [];
-  boardSize: number ) 9;
-  ActivePlayer: string = "X";
+  boardSize: number = 9;
+  activePlayer: string = "X";
   turnCount: number = 0;
   isGameRunning: boolean = false;
   isGameOver: boolean = false;
@@ -15,7 +15,7 @@ export class GameService {
 
   constructor() { 
     this.newGame();
-  }
+  }//fin constructor
 
   newGame(){
       this.activePlayer = "X";
@@ -24,36 +24,42 @@ export class GameService {
       this.isGameOver = false;
       this.winner = false;
       this.board = this.createBoard();
-  }
+  }//fin newGame
 
   createBoard(){
     let board = [];
     for (let i = 0; i < 9; i++){
       board.push({id:1, state:null});
-    };
+    };//fin for
     return board;
-  }
+  }//fin createBoard
 
   get getBoard(){
     return this.board;
-  }
+  }//fin getBoard
 
   set setBoard(board: any){
     this.board = [...board];
-}
+}//fin setBoard
 
 changePlayerTurn(squareCliked: any){
   this.updateBoard(squareCliked);
   if(!this.isGameOver) this.activePlayer = this.activePlayer === "X" ? "O" : "X";
   this.turnCount++;
   this.isGameOver = this.isGameOver ? true : false;
-}
+}//fin changePlayerTurn
 
 updateBoard(squareClicked: any){
-  this.board[squaredClicked.id].state = squareClicked.state;
+  this.board[squareClicked.id].state = squareClicked.state;
   if(this.winner){
     this.winner = true;
     this.isGameOver = false;
     this.isGameOver = true;
-  }
-}
+  }//fin if
+}//fin updateBoard
+
+get isWinner():boolean{
+  return this.checkDiag() || this.checkRows(this.board, "row") || this.checkRows(this.board, "col") ? true : false;
+}//fin is Winner
+
+}//fin class GameService
