cd8893c10b6d70f6e83fd401da90bab5d5000ba9
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-13 11:10:02 +0200

    board.component.ts OnInit, square.component.ts OnInit, Html board.component y square.component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.css']
 })
-export class SquareComponent {
+export class SquareComponent implements OnInit{
+  constructor(){ }
 
+  ngOnInit(): void {
+      
+  }
 }
