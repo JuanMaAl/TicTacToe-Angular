3f11dc5535643a4083f9f94dba8220a4283993d1
Juan Martinez Alonso <juanMaAl1986@gmail.com>
2023-06-14 11:53:39 +0200

    app.component.html gameService.active

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # TicTacToeAngular
 
+Se ha usado el tutorial de OctAcademy 
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.5.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GameService } from './game.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe-angular';
+  constructor (public gameService: GameService) { }
+    
 }
